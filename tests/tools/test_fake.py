ca88020d3c76521440289eb1f3f87c3b977df28a
Colley JM <jcolley@lpnhe.in2p3.fr>
2021-12-17 15:59:15 +0100

    solve exercice !

--- tests/tools/test_fake.py
+++ tests/tools/test_fake.py
@@ -4,17 +4,24 @@ v_a = np.array([1,5,6])
 v_b = np.array([2,2,9])
 
 
-
 def test_max_2_vectors():
-    assert  False
+    ret = max_2_vectors(v_a, v_b)
+    ret_ok = np.array([2,5,9])
+    np.testing.assert_array_equal(ret, ret_ok)
+    
 
 
 
 def test_min_2_vectors():
     ret = min_2_vectors(v_a, v_b)
     ret_ok = np.array([1,2,6])
+    print(ret)
     np.testing.assert_array_equal(ret, ret_ok)
 
 
 def test_min_2_vectors_pos():
-    assert False
+    ret = min_2_vectors(v_a, v_b)
+    ret_ok = np.array([1,2,6])
+    print(ret)
+    np.testing.assert_array_equal(ret, ret_ok)
+    
