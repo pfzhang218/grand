e39b32bd75d348a1763142f35b9148b713e60325
Colley JM <jcolley@lpnhe.in2p3.fr>
2021-12-20 23:53:16 +0100

    add correction

--- grand/tools/fake.py
+++ grand/tools/fake.py
@@ -1,49 +1,50 @@
-'''
+"""
 fake module for workshop GRAND 2021
-'''
+"""
 
 import numpy as np
 
 
 def max_2_vectors(v_a, v_b):
-    '''
+    """
     return max of same index
     .. example::
        a=[1,5,6]
        b=[2,2,9]
-       
+
        return=[2,5,9]
     :param v_a: numpy vector size n
     :param v_b: numpay vector size n
     :return: numpay vector size n
-    '''
-    v_ab= np.vstack((v_a,v_b))
+    """
+    v_ab = np.vstack((v_a, v_b))
     return v_ab.max(axis=0)
 
 
 def min_2_vectors(v_a, v_b):
-    '''
+    """
     return min of same index
     .. example::
        a=[1,5,6]
        b=[2,2,9]
-       
+
        return=[1,2,6]
-       
+
     :param v_a: numpy vector size n
     :param v_b: numpay vector size n
     :return: numpay vector size n
-    '''
-    v_ab= np.vstack((v_a,v_b))
+    """
+    v_ab = np.vstack((v_a, v_b))
     ret = v_ab.min(axis=0)
     return ret
-    
+
+
 def min_2_vectors_pos(v_a, v_b):
-    '''
+    """
     return min of same index value vector and 0 if negative
-    '''
+    """
     v_min = min_2_vectors(v_a, v_b)
-    idx_neg     = np.where(v_min < 0)[0]
-    v_min[idx_neg] = 0.0    
-    return v_a
-    
+    idx_neg = np.where(v_min < 0)[0]
+    print("idx_neg", idx_neg)
+    v_min[idx_neg] = 0.0
+    return v_min
--- tests/tools/test_fake.py
+++ tests/tools/test_fake.py
@@ -1,27 +1,30 @@
 from grand.tools.fake import *
 
-v_a = np.array([1,5,6])
-v_b = np.array([2,2,9])
+v_a = np.array([1, 5, 6])
+v_b = np.array([2, 2, 9])
 
 
 def test_max_2_vectors():
     ret = max_2_vectors(v_a, v_b)
-    ret_ok = np.array([2,5,9])
+    ret_ok = np.array([2, 5, 9])
     np.testing.assert_array_equal(ret, ret_ok)
-    
-
 
 
 def test_min_2_vectors():
     ret = min_2_vectors(v_a, v_b)
-    ret_ok = np.array([1,2,6])
+    ret_ok = np.array([1, 2, 6])
     print(ret)
     np.testing.assert_array_equal(ret, ret_ok)
 
 
-def test_min_2_vectors_pos():
-    ret = min_2_vectors(v_a, v_b)
-    ret_ok = np.array([1,2,6])
+def test_min_2_vectors_pos_1():
+    # all positive
+    ret = min_2_vectors_pos(v_a, v_b)
+    ret_ok = np.array([1, 2, 6])
     print(ret)
     np.testing.assert_array_equal(ret, ret_ok)
-    
+    # all negative
+    ret = min_2_vectors_pos(-v_a, -v_b)
+    ret_ok = np.zeros((3,))
+    print(ret)
+    np.testing.assert_array_equal(ret, ret_ok)
